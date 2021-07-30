aaad20415b677fe6bc4a19d650576cccda5e6da0
yilmazy17 <yilmazy17@itu.edu.tr>
2021-07-30 10:10:58 +0300

    final version

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,76 +30,73 @@ namespace Labyrinth
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.panel1 = new System.Windows.Forms.Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Labyrinth));
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(12, 38);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(606, 557);
-            this.panel1.TabIndex = 0;
             // 
             // label1
             // 
+            resources.ApplyResources(this.label1, "label1");
             this.label1.BackColor = System.Drawing.Color.DarkOliveGreen;
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(12, 6);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(288, 29);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Mirzalardan Kaç ";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // timer1
             // 
-            this.timer1.Enabled = true;
             this.timer1.Interval = 50;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label2
             // 
+            resources.ApplyResources(this.label2, "label2");
             this.label2.BackColor = System.Drawing.Color.DarkOliveGreen;
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(319, 6);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(288, 29);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "0";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Name = "label3";
             // 
             // Labyrinth
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(630, 607);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "Labyrinth";
-            this.Text = "Labyrinth";
             this.Load += new System.EventHandler(this.Labyrinth_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Labyrinth_KeyDown);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,41 +4,136 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Labyrinth
 {
+    
+   
     public partial class Labyrinth : Form
     {
+        
+        public class loc
+        {
+            public int x;
+            public int y;
+            public int sizes;
+        }
+        public List<loc> locs = new List<loc>();
         public double time = 0;
+        public int counter = 0;
+        private Random rand = new Random();
+        public Image images;
         public Labyrinth()
         {
+            this.KeyPreview = true;
             InitializeComponent();
         }
 
-        private void Labyrinth_Load(object sender, EventArgs e)
+        public static Image resizeImage(Image imgToResize, Size size)
         {
-            
+            return (Image)(new Bitmap(imgToResize, size));
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+
+
+            counter++;
             time = time + 0.05;
             label2.Text = time.ToString("0.00");
+            if (counter % 10 == 0)
+            {
+
+                Graphics g = this.CreateGraphics();
+                Image x = new Bitmap("1.jpg");
+                int a = rand.Next(50, 200);
+                x = resizeImage(x, new Size(a, a));
+                int xLoc = rand.Next(0, Width - a);
+                int yLoc = rand.Next(80, Height-a);
+                g.DrawImage(x, xLoc , yLoc);
+                
+                loc locat = new loc{x = xLoc, y = yLoc , sizes = a};
+                locs.Add(locat);
+
+
+
+            }
+            
         }
 
-        private void label1_Click(object sender, EventArgs e)
+
+        private void button1_Click(object sender, EventArgs e)
         {
-            if (timer1.Enabled)
+            timer1.Enabled = true;
+            button1.Hide();
+            label3.Show();
+        }
+
+        private void end_situations()
+        {
+
+        }
+
+        private void Labyrinth_Load(object sender, EventArgs e)
+        {
+            label3.Hide();
+        }
+
+        private void Labyrinth_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
             {
-                timer1.Enabled = false;
+                case Keys.Up:
+                    label3.Location = new Point(label3.Location.X, label3.Location.Y - 10);
+                    break;
+                case Keys.Down:
+                    label3.Location = new Point(label3.Location.X, label3.Location.Y + 10);
+                    break;
+                case Keys.Right:
+                    label3.Location = new Point(label3.Location.X+10, label3.Location.Y);
+                    break;
+                case Keys.Left:
+                    label3.Location = new Point(label3.Location.X - 10, label3.Location.Y);
+                    break;
             }
-            else
+
+            if (label3.Location.X < 3)
             {
-                timer1.Enabled = true;
+                label3.Location = new Point(Width + label3.Location.X, label3.Location.Y);
+            }
+            else if (label3.Location.X > Width)
+            {
+                label3.Location = new Point(0 + (-1 * (Width - label3.Location.X)), label3.Location.Y);
+            }
+
+            if (label3.Location.Y < 80)
+            {
+                label3.Location = new Point(label3.Location.X, Height + ((80-label3.Location.Y)));
+            } 
+            else if (label3.Location.Y > Height)
+            {
+                label3.Location = new Point(label3.Location.X, 80 + (( label3.Location.Y - Height)));
+            }
+            foreach (loc pos in locs)
+            {
+                if (label3.Location.X > pos.x && (pos.x + pos.sizes) > label3.Location.X && label3.Location.Y > pos.y && (pos.y + pos.sizes) > label3.Location.Y)
+                {
+                    timer1.Enabled = false;
+                    MessageBox.Show("Your point is : " + label2.Text + "s.");
+                    time = 0;
+                    Refresh();
+                    button1.Show();
+                    locs.Clear();
+                    label3.Hide();
+                    break;
+                }
+
             }
         }
+
     }
 }
